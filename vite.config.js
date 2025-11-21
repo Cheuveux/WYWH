a9beca4cd35830f1d7ec293a6c1343be239f351d
Cheveux <gaspard.szuszkin@gmail.com>
2025-11-21 17:44:52 +0100

    add multi-site for vite challah ca marche

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import multiHtml from "vite-plugin-multi-html";
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig (({mode}) => ({
-    base: mode === 'production' ? '/WYWH/' : '/', // nom du repo 
-    plugins: [
-        tailwindcss(),
-    ]
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/WYWH/' : '/', // Chemin correct pour GitHub Pages
+  plugins: [
+    tailwindcss(),
+    multiHtml({
+      pages: {
+        index: './src/index.html',
+        music: './src/music.html',
+        photo: './src/photo.html',
+        providers: './src/providers.html',
+      },
+    }),
+  ],
 }));
