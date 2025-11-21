8c59f1be8a815be669a0950050057d0a2c733f3c
Cheveux <gaspard.szuszkin@gmail.com>
2025-11-21 18:04:22 +0100

    ntm

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig (({mode}) => ({
-    base: mode === 'production' ? '/WYWH/' : '/', // nom du repo 
-    plugins: [
-        tailwindcss(),
-    ]
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/WYWH/' : '/', // Chemin correct pour GitHub Pages
+  plugins: [
+    tailwindcss(),
+    createHtmlPlugin({
+      minify: true, // Minifie les fichiers HTML
+      pages: {
+        index: './src/index.html',
+        music: './src/music.html',
+        photo: './src/photo.html',
+        providers: './src/providers.html',
+      },
+    }),
+  ],
 }));
