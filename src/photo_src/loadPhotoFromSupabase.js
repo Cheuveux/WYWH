e57868afad69a4faafbc5a28162be6ae4ad2bc52
Cheveux <gaspard.szuszkin@gmail.com>
2026-02-23 20:05:10 +0100

    link with artist with the photo

--- src/photo_src/loadPhotoFromSupabase.js
+++ src/photo_src/loadPhotoFromSupabase.js
@@ -11,12 +11,18 @@ export async function loadPhotoFromSupabase() {
             const { data, error } = await supabase
                 .from('wywh_photo')
                 .select(`
-                    id,
-					artist, 
+                    photo_id,
+					artist_id, 
 					photo_back,
 					photo_front,
 					localisation,
-					created_at
+					created_at,
+					wywh_photo_artist(
+					artistes (
+						id,
+						name
+					)
+					)
                 `)
                 // .order('order', { ascending: true });
             
@@ -28,17 +34,23 @@ export async function loadPhotoFromSupabase() {
         console.error("Erreur lors du chargement des tracks :", error);
         return [];
     }
+	
 }
 
 // Generation du HTML pour carte postale photo
 export function generatePhotoCard(photo) {
+	console.log('photo: ', photo);
+	const artistNames = photo.wywh_photo_artist
+	?.map(pa => pa.artistes?.name)
+	.filter(Boolean)
+	.join(', ') || 'Artiste inconnu';
 	return `
 		<div class="swiper-slide">
 			<div class="card" data-content="${photo.photo_front}|${photo.photo_back}">
 				<div class="card-inner"></div>
 			</div>
 			<h2 class="project-info">
-				<a href= "" class="artist-info">${photo.artist}|| 'Artiste inconnu'</a>
+				<a href= "" class="artist-info">${artistNames}</a>
 				<p>-</p>
 				<a href=-"" clas="location">${photo.localisation}</a>
 			</h2>
